28ac62520406dae46318193f9464661ce16ac223
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-01-30 10:50:01 -0300

    style: define min heigth

--- src/components/ScreenComponent/StyledComponents/index.ts
+++ src/components/ScreenComponent/StyledComponents/index.ts
@@ -4,10 +4,12 @@ export const ScreenContainer = styled.div`
     width: 260px;
     padding: 16px;
     background: #000;
+    min-height: 87px;
 
     > div {
         color: #808080;
         font-size: 16px;
+        word-break:break-all;
     }
 
     > div:last-child {
